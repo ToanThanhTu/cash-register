f3b8dfe7c8ffda4267438e483ae023c49eaf840b
Trevor <toantu3005@gmail.com>
2024-06-13 17:03:12 +1000

    Completed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const purchaseBtn = document.getElementById("purchase-btn");
 const priceScreen = document.querySelector(".price-screen");
 const cashInDrawer = document.getElementById("register-cash");
 
-let price = 3.99;
+let price = 3.26;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -82,35 +82,27 @@ const updateChangeDue = (array, status) => {
   changeDue.innerHTML = "";
   changeDue.innerHTML += status;
 
-  if (status === statuses["open"]) {
+  if (status === statuses["open"] || status === statuses["closed"]) {
     array.forEach((row) => {
       changeDue.innerHTML += `<p>${row[0]}: $${row[1]}</p>`;
     });
   }
-  
+
   updateCashInDrawer();
 }
 
 const getChange = () => {
-  let totalCashInDrawer = cid.reduce((acc, row) => acc + row[1], 0).toFixed(2);
-  console.log("totalCashInDrawer:", totalCashInDrawer);
+  let totalCashInDrawer = Number(cid.reduce((acc, row) => acc + row[1], 0).toFixed(2));
 
   let statusText = "";
   let totalChange = 0;
   let changes = [];
   let changeNeeded = Number((cashInput.value - price).toFixed(2));
-  console.log("Change needed: ", changeNeeded);
 
-  if (totalCashInDrawer < price || totalCashInDrawer < changeNeeded) {
-    console.log("-------------------");
-    console.log("Insufficient Funds, drawer cash less than price or change needed");
-    console.log("-------------------");
+  if (totalCashInDrawer < changeNeeded) {
     statusText = statuses["insufficient"];
   } else {
     for (let i = cid.length - 1; i >= 0; i--) {
-      console.log("Change needed: ", changeNeeded);
-      console.log("Current unit: ", cid[i][0]);
-      console.log("Current unit value: ", currencyUnits[cid[i][0]]);
       const currentUnitValue = Number(currencyUnits[cid[i][0]].toFixed(2));
       let currentUnitTotalChange = 0;
 
@@ -119,18 +111,12 @@ const getChange = () => {
         changeNeeded = Number((changeNeeded - currentUnitValue).toFixed(2));
         cid[i][1] = Number((cid[i][1] - currentUnitValue).toFixed(2));
         currentUnitTotalChange = Number((currentUnitTotalChange + currentUnitValue).toFixed(2));
-
-        console.log("---Total change so far: ", totalChange);
-        console.log("---Change needed so far: ", changeNeeded);
-        console.log("---Current Unit remaining amount: ", cid[i][1]);
       }
 
       if (currentUnitTotalChange) {
         const currentChange = [cid[i][0], currentUnitTotalChange];
         changes.push(currentChange);
-        console.log("Changes:", changes);
-      } else {
-        console.log("---SKIP---");
+        console.log("changes pushed: ", changes);
       }
     }
   }
@@ -138,14 +124,10 @@ const getChange = () => {
   totalCashInDrawer = cid.reduce((acc, row) => acc + row[1], 0).toFixed(2);
 
   if (changeNeeded > 0) {
-    console.log("-------------------");
-    console.log("Insufficient Funds, not enough change");
-    console.log("-------------------");
     statusText = statuses["insufficient"];
   } else if (totalCashInDrawer < price) {
     statusText = statuses["closed"];
   } else {
-    console.log("Changes list: ", changes);
     statusText = statuses["open"];
   }
 
@@ -153,13 +135,13 @@ const getChange = () => {
 }
 
 const processInput = () => {
-  if (cashInput.value < price) {
+  if (Number(cashInput.value) < price) {
     alert("Customer does not have enough money to purchase the item");
     return;
   }
 
-  if (cashInput.value === price) {
-    alert("No change due - customer paid with exact cash");
+  if (Number(cashInput.value) === price) {
+    updateChangeDue([], "No change due - customer paid with exact cash");
     return;
   }
 
